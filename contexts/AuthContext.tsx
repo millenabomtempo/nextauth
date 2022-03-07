9365508b4af5a9b561e93d71b1e1a70313e14ef8
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-03-07 18:42:12 -0300

    retrieving authentication state

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,6 +1,6 @@
 import Router from "next/router"
-import { setCookie } from "nookies"
-import { createContext, ReactNode, useState } from "react"
+import { parseCookies, setCookie } from "nookies"
+import { createContext, ReactNode, useEffect, useState } from "react"
 import { api } from "../services/api"
 
 type User = {
@@ -30,6 +30,19 @@ export function AuthProvider({children}: AuthProviderProps) {
   const [user, setUser] = useState<User>()
   const isAuthenticated = !!user
 
+  useEffect(() => {
+    const {'nextauth.token': token} = parseCookies()
+
+    if(token) {
+      api.get('/me').then(response => {
+        const { email, permissions, roles } = response.data
+
+        setUser({email, permissions, roles})
+      })
+    }
+
+  },[])
+
   async function signIn({email, password}: SignInCredentials){
     try {
       const response = await api.post('sessions', {email, password})
@@ -40,7 +53,7 @@ export function AuthProvider({children}: AuthProviderProps) {
         maxAge: 60 * 60 * 24 * 30, // 30 days
         path: '/'
       })
-      
+
       setCookie(undefined, 'nextauth.refreshToken', refreshToken, {
         maxAge: 60 * 60 * 24 * 30, // 30 days
         path: '/'
@@ -51,6 +64,8 @@ export function AuthProvider({children}: AuthProviderProps) {
         permissions,
         roles
       })
+
+      api.defaults.headers['Authorization'] = `Bearer ${token}`
 
       Router.push('/dashboard')
 
