9c6d24bf974e95592311483cb1e6f5933e044c8d
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-03-08 14:00:45 -0300

    performing automatic logout

--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from "axios";
 import { parseCookies, setCookie } from "nookies";
+import { signOut } from "../contexts/AuthContext";
 
 let cookies = parseCookies()
 let isRefreshing = false
@@ -69,7 +70,9 @@ api.interceptors.response.use(response => {
 
 
     } else {
-      //desloga o usuario
+      signOut()
     }
   }
+
+  return Promise.reject(error)
 })
