fe0eeaf335b5bad1389ca26ae6e20de1c9e6bc42
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-03-11 14:17:44 -0300

    Validating visitor

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,7 @@ import { parseCookies } from 'nookies'
 import { FormEvent, useContext, useState } from 'react'
 import { AuthContext } from '../contexts/AuthContext'
 import styles from '../styles/Home.module.css'
+import { withSSRGuest } from '../utils/withSSRGuest'
 
 const Home: NextPage = () => {
   const [email, setEmail] = useState('')
@@ -33,19 +34,9 @@ const Home: NextPage = () => {
 export default Home
 
 
-export const getServerSideProps: GetServerSideProps =async (ctx) => {
-  const cookies = parseCookies(ctx)
-
-  if(cookies['nextauth.token']) {
-    return {
-      redirect: {
-        destination: '/dashboard',
-        permanent: false,
-      }
-    }
-  }
-
+export const getServerSideProps = withSSRGuest( async (ctx) => {
+  
   return {
     props: {}
   }
-}
+})
