660a72cf36c1e917d2fcfe2528d5f9cebce750bd
Millena Bomtempo <millena.bomtempo@gmail.com>
2022-03-11 14:05:02 -0300

    Retrieving token on server-side

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
+import { parseCookies } from 'nookies'
 import { FormEvent, useContext, useState } from 'react'
 import { AuthContext } from '../contexts/AuthContext'
 import styles from '../styles/Home.module.css'
@@ -30,3 +31,21 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+
+export const getServerSideProps: GetServerSideProps =async (ctx) => {
+  const cookies = parseCookies(ctx)
+
+  if(cookies['nextauth.token']) {
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false,
+      }
+    }
+  }
+
+  return {
+    props: {}
+  }
+}
